4c45f58f3e69a36d19b4f482865fef1b6bf10b2d
Zschott <schottzane@gmail.com>
2024-06-06 13:38:54 -0400

    Adds minimum viable function and icons.

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// http://api.weatherapi.com/v1/forecast.json?key=5fca86161e294414a6b221028240306&q=London&days=3&aqi=no&alerts=no
-// `http://api.weatherapi.com/v1/forecast.json?key=5fca86161e294414a6b221028240306&q=${location)&days=3&aqi=no&alerts=no`
-
-/*  console.log(`city: ${city}`)
-    console.log(`current condition: ${conditionText}`)
-    console.log(`current temp: ${current}`)
-    console.log(`max temp: ${max}`)
-    console.log(`min temp: ${min}`) */
-
+const display = document.getElementById('display')
 const forecasts = []
 
 function Forecast(city, currentTemp, conditions, max, min, date, icon){
@@ -15,7 +7,6 @@ function Forecast(city, currentTemp, conditions, max, min, date, icon){
     this.conditions = conditions
     this.max = max;
     this.min = min;
-    this.condition = condition;
     this.date = date;
     this.icon = icon;
 }
@@ -26,43 +17,60 @@ async function stageForecast(){
     const weatherData = await response.json();
     console.log(weatherData)
     let city = weatherData.location.name; 
-    let current = weatherData.current.temp_f;
-    let conditionText = weatherData.current.condition.text;
+    let currentTemp = weatherData.current.temp_f;
+    let conditions = weatherData.current.condition.text;
     let max = weatherData.forecast.forecastday[0].day.maxtemp_f;
     let min = weatherData.forecast.forecastday[0].day.mintemp_f;
     let date = weatherData.forecast.forecastday[0].date;
-    let icon = weatherData.current.condition.icon;
-    return new Forecast(city, current, conditionText, max, min, date, icon)
+    let icon = removeDomain(weatherData.current.condition.icon);
+    return new Forecast(city, currentTemp, conditions, max, min, date, icon)
 }
 
-function addToArray(){
-    const newForecast = stageForecast()
+function removeDomain(str) {
+    return str.replace(/^.{20}/, '.');
+}
+
+async function addToArray(){
+    clearForecasts()
+    const newForecast =  await stageForecast()
     forecasts.push(newForecast)
+    console.log(forecasts[0])
     displayForecasts()
 }
 
 function displayForecasts(){
-    for(i=0; i < forecasts.length ; i++)
+    for(i=0; i < forecasts.length ; i++){
         createForecastCard(forecasts[i])
-        console.log(forecasts[i])
+    }
 }
 
-function createForecastCard(){
+function clearForecasts(){
+    forecasts.splice(0, 1)
+    display.textContent = ''
+}
+
+function createForecastCard(forecast){
+    console.log(forecast)
     const content = document.getElementById('display')
     const forecastCard = document.createElement('div')
     const forecastCity = document.createElement('div')
+    const forecastImg = document.createElement('img')
     const forecastCondition = document.createElement('div') // temp and condition
-    const forecastTemps = document.createElement('div')
+    const forecastHigh = document.createElement('div')
+    const forecastLow = document.createElement('div')
 
-    forecastCity.textContent = `${forecast.city}`
-    forecastCondition.textContent = `${forecast.current}, ${forecast.condition}`
-    forecastTemps.textContent = `Hi: ${forecast.max}F
-                                Low: ${forecast.min}F`
+    forecastCity.textContent =  `${forecast.city}`
+    forecastImg.src = `${forecast.icon}`
+    forecastCondition.textContent =  `${forecast.currentTemp} F, ${forecast.conditions}`
+    forecastHigh.textContent =  `High: ${forecast.max} F`
+    forecastLow.textContent = `Low: ${forecast.min} F`
 
     forecastCard.classList.add('card')
     forecastCard.appendChild(forecastCity)
+    forecastCard.appendChild(forecastImg)
     forecastCard.appendChild(forecastCondition)
-    forecastCard.appendChild(forecastTemps)
+    forecastCard.appendChild(forecastHigh)
+    forecastCard.appendChild(forecastLow)
 
     content.appendChild(forecastCard)
 }
